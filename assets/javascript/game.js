6b44ef3eebc64ecd6bdd2db208e28d80b21da42a
meachamm92 <meacham.space@gmail.com>
2020-09-22 16:58:40 -0500

    added background image and styling to most of app, also added possession indicator and now alerts user who won after pressing reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,8 @@ const shootButtonTwo = document.querySelector("#teamtwo-shoot-button");
 
 let numShotsOne = document.querySelector("#teamone-numshots");
 let numGoalsOne = document.querySelector("#teamone-numgoals");
+const resetSpan = document.querySelector("#numResetSpan");
+let numResets = document.querySelector("#num-resets");
 
 let numShotsTwo = document.querySelector("#teamtwo-numshots");
 let numGoalsTwo = document.querySelector("#teamtwo-numgoals");
@@ -15,8 +17,13 @@ const teamTwoName = document.querySelector("#teamTwo");
 const swishSound = document.querySelector("#swish");
 const brickSound = document.querySelector("#brick");
 
+const leftDiv = document.querySelector("#left");
+const rightDiv = document.querySelector("#right");
+
+const fieldGoalRate = 60;
+
 // Ask user for team names and assign to object variables and check
-// for valid user inputs
+// // for valid user inputs
 teamOneName.textContent = prompt("What is the name of Team One?");
 while (teamOneName.textContent == "")
   teamOneName.textContent = prompt("Hey! Enter a valid name for Team One!!!");
@@ -25,12 +32,25 @@ teamTwoName.textContent = prompt("What is the name of Team Two?");
 while (teamTwoName.textContent == "")
   teamTwoName.textContent = prompt("Hey! Enter a valid name for Team Two!!!");
 
+leftDiv.classList.toggle("possessionIndicator");
+
 // Add reset click event; will reset both teams' shots taken and goals
 resetButton.addEventListener("click", function () {
+  console.log(typeof parseInt(numGoalsOne.innerHTML));
+
+  if (parseInt(numGoalsOne.innerHTML, 10) > parseInt(numGoalsTwo.innerHTML, 10))
+    alert(`The ${teamOneName.textContent} have won the game!`);
+  else if (parseInt(numGoalsTwo.innerHTML) > parseInt(numGoalsOne.innerHTML))
+    alert(`The ${teamTwoName.textContent} have won the game!`);
+  // why is this else not working??
+  else "The two teams have tied.";
+
   numShotsOne.innerHTML = 0;
   numShotsTwo.innerHTML = 0;
   numGoalsOne.innerHTML = 0;
   numGoalsTwo.innerHTML = 0;
+  resetSpan.classList.remove("hide");
+  numResets.innerHTML++;
 });
 
 // Add shoot click event; will succeed or fail based % chance compared to random number
@@ -40,18 +60,17 @@ shootButtonOne.addEventListener("click", function () {
   // Create a variable to be compared with a random number. This value will
   // essentially serve as field goal percentage * 100. If the random number is higher
   // than our coded field goal percentage, then the shot will miss.
-  const fieldGoalRate = 60;
   let random = generateRandomNumber(100);
   console.log(random);
 
   if (fieldGoalRate >= random) {
     numGoalsOne.innerHTML++;
     playSwish();
-    alert("The " + teamOneName.textContent + " have scored!");
-  } else {
-      playBrick();
-      alert("The " + teamOneName.textContent + " MISSED!!! OUCH!!");
-  }
+  } else playBrick();
+
+  leftDiv.classList.toggle("possessionIndicator");
+  leftDiv.classList.toggle("test");
+  rightDiv.classList.toggle("possessionIndicator");
 });
 
 // Add shoot click event; will succeed or fail based % chance compared to random number
@@ -61,36 +80,31 @@ shootButtonTwo.addEventListener("click", function () {
   // Create a variable to be compared with a random number. This value will
   // essentially serve as field goal percentage * 100. If the random number is higher
   // than our coded field goal percentage, then the shot will miss.
-  const fieldGoalRate = 60;
   let random = generateRandomNumber(100);
   console.log(random);
 
   if (fieldGoalRate >= random) {
     numGoalsTwo.innerHTML++;
     playSwish();
-    alert("The " + teamTwoName.textContent + " have scored!");
-  } else {
-      playBrick();
-      alert("The " + teamTwoName.textContent + " MISSED!!! OUCH!!");
-  }
+  } else playBrick();
+
+  leftDiv.classList.toggle("possessionIndicator");
+  rightDiv.classList.toggle("possessionIndicator");
 });
-
-
 
 // Create a play functions which will play either "swish" or "brick"
 // if the team scores or not
 function playSwish() {
-    swishSound.play();
+  swishSound.play();
 }
 
 function playBrick() {
-    brickSound.play();
+  brickSound.play();
 }
 
-// Function will create a random number, which will be compared to the 
+// Function will create a random number, which will be compared to the
 // variable field goal rate
 function generateRandomNumber(max) {
-    max++;
-    let number = Math.floor(Math.random() * max);
-    return number;
+  let number = Math.floor(Math.random() * max) + 1;
+  return number;
 }
