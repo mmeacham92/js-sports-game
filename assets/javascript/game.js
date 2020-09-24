703d6292d58f377124ca0506fb88857c00c479b2
meachamm92 <meacham.space@gmail.com>
2020-09-24 11:45:28 -0500

    added input elements for team names; added play and play again button along with functionality; fixed some styling problems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+// -------------------------------VARIABLES-------------------------------------
+
 // Initialize and assign needed object variables
 const resetButton = document.querySelector("#reset-button");
 const shootButtonOne = document.querySelector("#teamone-shoot-button");
@@ -7,6 +9,8 @@ let numShotsOne = document.querySelector("#teamone-numshots");
 let numGoalsOne = document.querySelector("#teamone-numgoals");
 const resetSpan = document.querySelector("#numResetSpan");
 let numResets = document.querySelector("#num-resets");
+
+const inputs = document.querySelector("#inputs");
 
 let numShotsTwo = document.querySelector("#teamtwo-numshots");
 let numGoalsTwo = document.querySelector("#teamtwo-numgoals");
@@ -20,77 +24,134 @@ const brickSound = document.querySelector("#brick");
 const leftDiv = document.querySelector("#left");
 const rightDiv = document.querySelector("#right");
 
+let gameTitle = document.querySelector("#game-title");
+let winner = document.querySelector("#winner");
+
+// Game variables
 const fieldGoalRate = 60;
+let running = true;
 
 // Ask user for team names and assign to object variables and check
 // // for valid user inputs
-teamOneName.textContent = prompt("What is the name of Team One?");
+/* teamOneName.textContent = prompt("What is the name of Team One?");
 while (teamOneName.textContent == "")
   teamOneName.textContent = prompt("Hey! Enter a valid name for Team One!!!");
 
 teamTwoName.textContent = prompt("What is the name of Team Two?");
 while (teamTwoName.textContent == "")
-  teamTwoName.textContent = prompt("Hey! Enter a valid name for Team Two!!!");
+  teamTwoName.textContent = prompt("Hey! Enter a valid name for Team Two!!!"); */
 
-leftDiv.classList.toggle("possessionIndicator");
+leftDiv.classList.add("possessionIndicator");
+
+numGoalsTwo.innerHTML = 9;
+
+// ---------------------------EVENT LISTENERS -------------------------------------------
 
 // Add reset click event; will reset both teams' shots taken and goals
 resetButton.addEventListener("click", function () {
-  console.log(typeof parseInt(numGoalsOne.innerHTML));
+  if (resetButton.textContent == "RESET") {
+    reset();
+    toggleTeamDivs();
+    numResets.innerHTML++;
+  } else if (resetButton.innerHTML == "PLAY") {
+    // Grab team names from user input fields
+    let teamOneInput = document.querySelector("#teamOneInput");
+    let teamTwoInput = document.querySelector("#teamTwoInput");
+    // Assign the text content of the team name elements to the value of the inputs
+    teamOneName.textContent = teamOneInput.value;
+    teamTwoName.textContent = teamTwoInput.value;
+    // 'Play' button now becomes a 'Reset' button
+    resetButton.textContent = "RESET";
+    // hide the input elements and unhide the left and right elements
+    inputs.classList.add("hide");
+    toggleTeamDivs();
+  } else {
+    winner.classList.add("hide");
+    gameTitle.classList.remove("hide");
+    reset();
+    resetSpan.classList.add("hide");
+  }
+});
 
-  if (parseInt(numGoalsOne.innerHTML, 10) > parseInt(numGoalsTwo.innerHTML, 10))
-    alert(`The ${teamOneName.textContent} have won the game!`);
-  else if (parseInt(numGoalsTwo.innerHTML) > parseInt(numGoalsOne.innerHTML))
-    alert(`The ${teamTwoName.textContent} have won the game!`);
-  // why is this else not working??
-  else "The two teams have tied.";
+// Add shoot click event; will succeed or fail based % chance compared to random number
+shootButtonOne.addEventListener("click", function () {
+  // check to make sure its the team's turn
+  if (leftDiv.classList.contains("possessionIndicator") && running) {
+    // Increase shots taken value by one
+    numShotsOne.innerHTML++;
+    // Create a variable to be compared with a random number. This value will
+    // essentially serve as field goal percentage * 100. If the random number is higher
+    // than our coded field goal percentage, then the shot will miss.
+    let random = generateRandomNumber(100);
+    console.log(random);
 
+    if (fieldGoalRate >= random) {
+      numGoalsOne.innerHTML++;
+      playSwish();
+    } else playBrick();
+
+    if (Number(numGoalsOne.innerHTML) === 10) {
+      gameOver();
+      winner.innerHTML = `${teamOneName.textContent} has won the game!`;
+    } else changePossession();
+  } else alert("Hey! It's not your turn!");
+});
+
+// Add shoot click event; will succeed or fail based % chance compared to random number
+shootButtonTwo.addEventListener("click", function () {
+  if (rightDiv.classList.contains("possessionIndicator") && running) {
+    // Increase shots taken value by one
+    numShotsTwo.innerHTML++;
+    // Create a variable to be compared with a random number. This value will
+    // essentially serve as field goal percentage * 100. If the random number is higher
+    // than our coded field goal percentage, then the shot will miss.
+    let random = generateRandomNumber(100);
+    console.log(random);
+
+    if (fieldGoalRate >= random) {
+      numGoalsTwo.innerHTML++;
+      playSwish();
+    } else playBrick();
+
+    if (Number(numGoalsTwo.innerHTML) === 10) {
+      gameOver();
+      winner.innerHTML = `${teamTwoName.textContent} has won the game!`;
+    } else changePossession();
+  } else alert("Hey! It's not your turn!");
+});
+
+// --------------------------FUNCTIONS-----------------------------------------
+
+// reset game defaults
+function reset() {
+  resetButton.innerHTML = "RESET";
+  winner.classList.add("hide");
   numShotsOne.innerHTML = 0;
   numShotsTwo.innerHTML = 0;
   numGoalsOne.innerHTML = 0;
   numGoalsTwo.innerHTML = 0;
   resetSpan.classList.remove("hide");
-  numResets.innerHTML++;
-});
+  leftDiv.classList.add("possessionIndicator");
+  rightDiv.classList.remove("possessionIndicator");
+  toggleTeamDivs();
+  running = true;
+}
 
-// Add shoot click event; will succeed or fail based % chance compared to random number
-shootButtonOne.addEventListener("click", function () {
-  // Increase shots taken value by one
-  numShotsOne.innerHTML++;
-  // Create a variable to be compared with a random number. This value will
-  // essentially serve as field goal percentage * 100. If the random number is higher
-  // than our coded field goal percentage, then the shot will miss.
-  let random = generateRandomNumber(100);
-  console.log(random);
+// stops running the game and shows the winning team
+function gameOver() {
+  running = false;
+  gameTitle.classList.add("hide");
+  winner.classList.remove("hide");
+  resetSpan.classList.add("hide");
+  resetButton.innerHTML = "PLAY AGAIN";
+  toggleTeamDivs();
+}
 
-  if (fieldGoalRate >= random) {
-    numGoalsOne.innerHTML++;
-    playSwish();
-  } else playBrick();
-
-  leftDiv.classList.toggle("possessionIndicator");
-  leftDiv.classList.toggle("test");
-  rightDiv.classList.toggle("possessionIndicator");
-});
-
-// Add shoot click event; will succeed or fail based % chance compared to random number
-shootButtonTwo.addEventListener("click", function () {
-  // Increase shots taken value by one
-  numShotsTwo.innerHTML++;
-  // Create a variable to be compared with a random number. This value will
-  // essentially serve as field goal percentage * 100. If the random number is higher
-  // than our coded field goal percentage, then the shot will miss.
-  let random = generateRandomNumber(100);
-  console.log(random);
-
-  if (fieldGoalRate >= random) {
-    numGoalsTwo.innerHTML++;
-    playSwish();
-  } else playBrick();
-
+// toggles the possession indicator class
+function changePossession() {
   leftDiv.classList.toggle("possessionIndicator");
   rightDiv.classList.toggle("possessionIndicator");
-});
+}
 
 // Create a play functions which will play either "swish" or "brick"
 // if the team scores or not
@@ -107,4 +168,9 @@ function playBrick() {
 function generateRandomNumber(max) {
   let number = Math.floor(Math.random() * max) + 1;
   return number;
+}
+
+function toggleTeamDivs() {
+  leftDiv.classList.toggle("hide");
+  rightDiv.classList.toggle("hide");
 }
